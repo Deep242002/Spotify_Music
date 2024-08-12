f17029738972acf916599e5f8695fe89eae0de21
Deep242002 <kamal24zone@gmail.com>
2024-08-13 02:05:44 +0530

    Update bot.py

--- Spotify_Music/core/bot.py
+++ Spotify_Music/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 class Spotify(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Starting Bot...")
+        LOGGER(__name__).info(f"Aru ♥️ Miss u...")
         super().__init__(
             name="Spotify_Music",
             api_id=config.API_ID,
